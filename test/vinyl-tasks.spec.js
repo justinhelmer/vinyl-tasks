56349799561507d8f093219307d5ef1f88eb85f9
justinhelmer <jhelmer25@gmail.com>
2016-02-14 20:25:47 -0800

    write spec for main module export

--- test/vinyl-tasks.spec.js
+++ test/vinyl-tasks.spec.js
@@ -1,11 +1,7 @@
 (function() {
   'use strict';
 
-  var _ = require('lodash');
-  var chai = require('chai');
-  var expect = chai.expect;
-  var sinon = require('sinon');
-  chai.use(require('sinon-chai'));
+  var expect = require('chai').expect;
 
   var create = require('../lib/create');
   var filter = require('../lib/filter');
@@ -14,7 +10,7 @@
 
   describe('vinyl-tasks (exported module)', function() {
     it('should export an object with a reference to all public interfaces', function() {
-      expect(_.keys(tasks)).to.eql(['create', 'filter', 'pipeline']);
+      expect(tasks).to.have.all.keys('create', 'filter', 'pipeline');
       expect(tasks.create).to.eq(create);
       expect(tasks.filter).to.eq(filter);
       expect(tasks.pipeline).to.eq(pipeline);
